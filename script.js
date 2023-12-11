933a9cf2c0e59a1b751e0bc6194879655bf68073
karthi5555 <105117281+karthi5555@users.noreply.github.com>
2023-12-11 09:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function  fizzBuzz(st, end){
+
+	for(let i=st; i<=end; i++){
+		//logic
+		if(i % 3 == 0 && i % 5 == 0){
+			alert("FizzBuzz")
+		}
+		else if(i % 3 == 0){
+			alert("Fizz")
+		}
+		else if(i % 5 == 0){
+			alert("Buzz")
+		}
+		else{
+			alert(i.toString())
+		}
+	}
+}
+fizzBuzz(1, 100)
